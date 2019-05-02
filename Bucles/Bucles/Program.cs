20d42401ee4da84a714d7781b0b28f4fcd7aef73
arrate ortiz <arrateoc@gmail.com>
2019-05-02 19:02:00 +0200

    ejercicios bucles

--- Bucles/Bucles/Program.cs
+++ Bucles/Bucles/Program.cs
@@ -6,7 +6,27 @@ namespace Bucles
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Escribe un programa que muestre en pantalla los números del 10 al 20(incluidos los dos) con una línea vacía entre ellos.
+            //for (int i = 10; i <= 20; i++)
+            //{
+            //    Console.WriteLine(i + "\n");
+            //    //Console.WriteLine();
+            //}
+
+
+            //int numero = 20;
+            //while (numero >= 10)
+            //{
+            //    Console.WriteLine(numero);
+            //    Console.WriteLine();
+            //    numero--;
+            //}
+
+            for(int i = 20; i >= 10; i--)
+            {
+                Console.WriteLine(i + "\n");
+               
+            }
         }
     }
 }
