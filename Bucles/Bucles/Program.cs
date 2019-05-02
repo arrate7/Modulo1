89a3e1a7cdca00bb0ea0caadc47c57797398091b
arrate ortiz <arrateoc@gmail.com>
2019-05-02 18:03:20 +0200

    .

---
+++ Bucles/Bucles/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bucles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
