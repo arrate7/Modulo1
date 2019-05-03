8690c6942b1bdd46a3a78920ccce7c9c424db803
arrate7 <43437355+arrate7@users.noreply.github.com>
2019-05-03 10:45:35 +0200

    .

--- Bucles/Bucles/Program.cs
+++ Bucles/Bucles/Program.cs
@@ -31,8 +31,40 @@ namespace Bucles
             //for(int i = 20; i >= 10; i--)
             //{
             //    Console.WriteLine(i + "\n");
-               
+
             //}
+
+            ////Ejercicio 3
+
+            //double suma = 0;
+            //double num;
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("Introduzca otro numero:");
+            //    num = Convert.ToDouble(Console.ReadLine());
+            //    suma = suma + num;
+            //}
+            //Console.WriteLine("La media de los numeros introducido es: " + suma / 10.0);
+
+            //Ejercicio 4
+            int total = 0;
+            int numero;
+            do
+            {
+                Console.WriteLine("introduce un numero");
+                numero = Convert.ToInt32(Console.ReadLine());
+                if (numero < 0)
+                {
+                    Console.WriteLine("Se ignoran los valores negativos");
+                }
+                else
+                {
+                    total += numero;
+                }
+            }
+            while (numero != 0);
+
+            Console.WriteLine(total);
         }
     }
 }
