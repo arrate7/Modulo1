8e59721cb6344d9e9be83b21bf2e86304263ecc4
arrate7 <43437355+arrate7@users.noreply.github.com>
2019-05-06 16:17:03 +0200

    .

--- Calculadora/Calculadora/Program.cs
+++ Calculadora/Calculadora/Program.cs
@@ -6,6 +6,143 @@ namespace Calculadora
     {
         static void Main(string[] args)
         {
+
+
+
+
+            //int numOperacion;
+            //do
+            //{
+            //    Console.WriteLine("*****************");
+            //    Console.WriteLine("   Calculadora");
+            //    Console.WriteLine("*****************");
+            //    Console.WriteLine("1. Suma");
+            //    Console.WriteLine("2. Resta");
+            //    Console.WriteLine("3. Multiplicación");
+            //    Console.WriteLine("4. Potencia");
+            //    Console.WriteLine("5. División");
+            //    Console.WriteLine("6. Raíz");
+            //    Console.WriteLine("7. Salir" + "\n");
+            //    Console.WriteLine("Introduce la opción deseada:");
+            //    numOperacion = Convert.ToInt32(Console.ReadLine());
+
+            //    double numero1 = 0;
+            //    double numero2 = 0;
+
+            //    if (numOperacion > 0 && numOperacion < 6)
+            //    {
+            //        Console.WriteLine("Introduce el primer número:");
+            //        numero1 = Convert.ToDouble(Console.ReadLine());
+            //        Console.WriteLine("Introduce el segundo número:");
+            //        numero2 = Convert.ToDouble(Console.ReadLine());
+            //    }
+
+            //    switch (numOperacion)
+            //    {
+            //        case 1:
+            //            Console.WriteLine("Operación seleccionada: Suma");
+            //            Console.WriteLine(numero1 + "+" + numero2 + "=" + (numero1 + numero2));
+            //            break;
+            //        case 2:
+            //            Console.WriteLine("Operación seleccionada: Resta");
+            //            Console.WriteLine(numero1 + "-" + numero2 + "=" + (numero1 - numero2));
+            //            break;
+            //        case 3:
+
+            //            Console.WriteLine("Operación seleccionada: Multiplicación");
+            //            Console.WriteLine(numero1 + "*" + numero2 + "=" + (numero1 * numero2));
+            //            break;
+            //        case 4:
+
+            //            Console.WriteLine("Operación seleccionada: Potencia");
+            //            Console.WriteLine(numero1 + " ^ " + numero2 + " = " + (Math.Pow(numero1, numero2)));
+            //            break;
+            //        case 5:
+
+            //            Console.WriteLine("Operación seleccionada: División");
+            //            Console.WriteLine(numero1 + "/" + numero2 + "=" + (numero1 / numero2));
+            //            break;
+            //        case 6:
+            //            Console.WriteLine("Introduce el número:");
+            //            numero1 = Convert.ToDouble(Console.ReadLine());
+            //            Console.WriteLine("Operación seleccionada: Raíz");
+            //            Console.WriteLine("|" + numero1 + "=" + (Math.Sqrt(numero1)));
+            //            break;
+            //        case 7:
+            //            Console.WriteLine("Hasta luego!");
+            //            break;
+            //        default:
+            //            Console.WriteLine("Opción " + numOperacion + " no disponible, vuelva a intentarlo");
+            //            break;
+            //    }
+            //} while (numOperacion != 7);
+
+
+
+            ///////PARTE 2: SOLO NÚMEROS POSITIVOS
+            //bool salir = false;
+            //do
+            //{
+            //    Console.WriteLine("Introduce una operación matemática:");
+            //    string operacion = Console.ReadLine(); //300+200
+            //    string simbolo = "";
+            //    int posicionSimbolo;
+
+            //    if (operacion.ToLower() == "q")
+            //    {
+            //        salir = true;
+            //    }
+            //    else
+            //    {
+
+            //        for (int i = 0; i < operacion.Length; i++)
+            //        {
+
+            //            if (!Char.IsDigit(Convert.ToChar(operacion.Substring(i, 1))))
+            //            {
+            //                simbolo = operacion.Substring(i, 1);
+            //            }
+
+            //        }
+
+            //        posicionSimbolo = operacion.IndexOf(simbolo);
+            //        double num1 = Convert.ToDouble(operacion.Substring(0, posicionSimbolo));
+            //        double num2 = Convert.ToDouble(operacion.Substring(posicionSimbolo + 1));
+            //        double resultado;
+            //        switch (simbolo)
+            //        {
+
+            //            case "+":
+            //                resultado = num1 + num2;
+            //                Console.WriteLine(resultado);
+            //                break;
+
+            //            case "-":
+            //                resultado = num1 - num2;
+            //                Console.WriteLine(resultado);
+            //                break;
+
+            //            case "*":
+            //                resultado = num1 * num2;
+            //                Console.WriteLine(resultado);
+            //                break;
+
+            //            case "/":
+            //                resultado = num1 / num2;
+            //                Console.WriteLine(resultado);
+            //                break;
+
+            //            default:
+            //                Console.WriteLine("Debes introducir una operación matemática: +, -, * o /.");
+            //                break;
+            //        }
+
+            //    }
+
+            //} while (!salir);
+
+            ///////////////////////////////VERSIÓN ARRATE PARTE 2
+            ///
             //    double valor1 = 0, valor2 = 0;
             //    string operacion = "";
             //    string simbolo = "";
@@ -108,139 +245,6 @@ namespace Calculadora
             //        Console.ReadLine();
             //    }
 
-
-
-            //int numOperacion;
-            //do
-            //{
-            //    Console.WriteLine("*****************");
-            //    Console.WriteLine("   Calculadora");
-            //    Console.WriteLine("*****************");
-            //    Console.WriteLine("1. Suma");
-            //    Console.WriteLine("2. Resta");
-            //    Console.WriteLine("3. Multiplicación");
-            //    Console.WriteLine("4. Potencia");
-            //    Console.WriteLine("5. División");
-            //    Console.WriteLine("6. Raíz");
-            //    Console.WriteLine("7. Salir" + "\n");
-            //    Console.WriteLine("Introduce la opción deseada:");
-            //    numOperacion = Convert.ToInt32(Console.ReadLine());
-
-            //    double numero1 = 0;
-            //    double numero2 = 0;
-
-            //    if (numOperacion > 0 && numOperacion < 6)
-            //    {
-            //        Console.WriteLine("Introduce el primer número:");
-            //        numero1 = Convert.ToDouble(Console.ReadLine());
-            //        Console.WriteLine("Introduce el segundo número:");
-            //        numero2 = Convert.ToDouble(Console.ReadLine());
-            //    }
-
-            //    switch (numOperacion)
-            //    {
-            //        case 1:
-            //            Console.WriteLine("Operación seleccionada: Suma");
-            //            Console.WriteLine(numero1 + "+" + numero2 + "=" + (numero1 + numero2));
-            //            break;
-            //        case 2:
-            //            Console.WriteLine("Operación seleccionada: Resta");
-            //            Console.WriteLine(numero1 + "-" + numero2 + "=" + (numero1 - numero2));
-            //            break;
-            //        case 3:
-
-            //            Console.WriteLine("Operación seleccionada: Multiplicación");
-            //            Console.WriteLine(numero1 + "*" + numero2 + "=" + (numero1 * numero2));
-            //            break;
-            //        case 4:
-
-            //            Console.WriteLine("Operación seleccionada: Potencia");
-            //            Console.WriteLine(numero1 + " ^ " + numero2 + " = " + (Math.Pow(numero1, numero2)));
-            //            break;
-            //        case 5:
-
-            //            Console.WriteLine("Operación seleccionada: División");
-            //            Console.WriteLine(numero1 + "/" + numero2 + "=" + (numero1 / numero2));
-            //            break;
-            //        case 6:
-            //            Console.WriteLine("Introduce el número:");
-            //            numero1 = Convert.ToDouble(Console.ReadLine());
-            //            Console.WriteLine("Operación seleccionada: Raíz");
-            //            Console.WriteLine("|" + numero1 + "=" + (Math.Sqrt(numero1)));
-            //            break;
-            //        case 7:
-            //            Console.WriteLine("Hasta luego!");
-            //            break;
-            //        default:
-            //            Console.WriteLine("Opción " + numOperacion + " no disponible, vuelva a intentarlo");
-            //            break;
-            //    }
-            //} while (numOperacion != 7);
-
-            bool salir = false;
-            do
-            {
-                Console.WriteLine("Introduce una operación matemática:");
-                string operacion = Console.ReadLine(); //300+200
-                string simbolo = "";
-                int posicionSimbolo;
-
-                if (operacion.ToLower() == "q")
-                {
-                    salir = true;
-                }
-                else
-                {
-
-                    for (int i = 0; i < operacion.Length; i++)
-                    {
-
-                        if (!Char.IsDigit(Convert.ToChar(operacion.Substring(i, 1))))
-                        {
-                            simbolo = operacion.Substring(i, 1);
-                        }
-
-                    }
-
-                    posicionSimbolo = operacion.IndexOf(simbolo);
-                    double num1 = Convert.ToDouble(operacion.Substring(0, posicionSimbolo));
-                    double num2 = Convert.ToDouble(operacion.Substring(posicionSimbolo + 1));
-                    double resultado;
-                    switch (simbolo)
-                    {
-
-                        case "+":
-                            resultado = num1 + num2;
-                            Console.WriteLine(resultado);
-                            break;
-
-                        case "-":
-                            resultado = num1 - num2;
-                            Console.WriteLine(resultado);
-                            break;
-
-                        case "*":
-                            resultado = num1 * num2;
-                            Console.WriteLine(resultado);
-                            break;
-
-                        case "/":
-                            resultado = num1 / num2;
-                            Console.WriteLine(resultado);
-                            break;
-
-                        default:
-                            Console.WriteLine("Debes introducir una operación matemática: +, -, * o /.");
-                            break;
-                    }
-
-                }
-
-            } while (!salir);
-
-            string[] animales = { "gato","perro"};
-            
-            
         }
 
     }
