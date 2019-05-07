575d549e53df1aa1ca541993d5bc1802e28cc01d
arrate7 <43437355+arrate7@users.noreply.github.com>
2019-05-07 18:15:09 +0200

    .

---
+++ Metodos/Ejercicio1y2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ejercicio1y2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //RepeatHola();
+            //Console.WriteLine(" Introduce los  numeros 5 y 3 ");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+            EscribeHola(3);
+            Console.WriteLine("");
+            EscribeHola(5);
+
+        }
+        public static void RepeatHola()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Hola");
+            }
+        }
+        public static void EscribeHola(int veces)
+        {
+
+            for (int i = 0; i < veces; i++)
+            {
+                Console.WriteLine("Hola");
+            }
+        }
+    }
+}
---
+++ Metodos/Metodos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Metodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ejercicio 1
+            SumarDosNumeros(2,3);
+            //Ejercicio 2
+            SumarDosNumeros(2,6);
+
+       
+        }
+
+        public static void SumarDosNumeros(int num1, int num2)
+        {
+            Console.WriteLine(num1+num2);
+        }
+
+
+    }
+}
