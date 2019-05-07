d15a8084968573c44a8905481f85c4f8be7fb013
arrate7 <43437355+arrate7@users.noreply.github.com>
2019-05-07 18:40:41 +0200

    .

---
+++ Metodos/Ejercicio3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int resultado = Suma(5, 9);
+            Console.WriteLine(resultado);
+        }
+
+        public static int Suma(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
