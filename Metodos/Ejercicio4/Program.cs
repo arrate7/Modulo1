0489f20a0ecc72bb84cbf3430b03596dc55f57ec
arrate7 <43437355+arrate7@users.noreply.github.com>
2019-05-08 09:32:48 +0200

    .

---
+++ Metodos/Ejercicio4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ejercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce un número:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Introduce otro número:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int resultado = Suma(num1, num2);
+
+            Console.WriteLine(resultado);
+        }
+
+        public static int Suma(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
