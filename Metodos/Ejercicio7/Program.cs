43589125a908ac6d170e21b5e38c46e6b1e80b29
arrate7 <43437355+arrate7@users.noreply.github.com>
2019-05-08 12:55:55 +0200

    .

---
+++ Metodos/Ejercicio7/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Ejercicio7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int years;
+
+            do
+            {
+                Console.WriteLine("Escribe un año");
+                years = Convert.ToInt32(Console.ReadLine());
+
+                //bool bisiesto = Year(years);
+
+                if (Year(years))
+                {
+                    Console.WriteLine("El año es bisiesto");
+                }
+                else
+                {
+                    Console.WriteLine("El año no es bisiesto");
+                }
+
+            } while (years != 0);
+        }
+        public static bool Year(int num1)
+        {
+            //if (DateTime.IsLeapYear(num1))
+            //{
+            //    return true;
+            //}
+            //else
+            //{
+            //    return false;
+            //}
+
+            return DateTime.IsLeapYear(num1);
+        }
+    }
+}
