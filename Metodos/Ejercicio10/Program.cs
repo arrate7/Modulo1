bafb3f9f1d6581270354de580103c3430f123d34
arrate7 <43437355+arrate7@users.noreply.github.com>
2019-05-08 16:59:59 +0200

    .

---
+++ Metodos/Ejercicio10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ejercicio10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dime una palabra");
+            string p1 = Console.ReadLine();
+            Console.WriteLine("Tu palabra contiene " + Computo(p1));
+        }
+        public static int Computo(string pal1)
+        {
+            int palabras = 0;
+            for (int i = 0; i < pal1.Length; i++)
+            {
+                string letra = pal1.Substring(i, 1);
+                //char caracter = pal1[i];
+                if (letra == " ")
+                {
+                    palabras++;
+                }
+            }
+            palabras++;
+            return palabras;
+        }
+    }
+}
