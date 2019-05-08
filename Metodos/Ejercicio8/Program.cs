7984e6668abda92e88d340cb3b341fc6c38498e2
arrate7 <43437355+arrate7@users.noreply.github.com>
2019-05-08 13:36:04 +0200

    .

---
+++ Metodos/Ejercicio8/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Ejercicio8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce un numero cualquiera");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            //if (ParImpar(num))
+            //{
+            //    Console.WriteLine("El numero es par");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("El numero es impar");
+            //}
+            Console.WriteLine(ParImpar(num));
+        }
+
+
+        public static string ParImpar(int num)
+        {
+            if (num % 2 == 0)
+            {
+                return "Es par";
+            }
+
+            else
+            {
+                return "Es impar";
+            }
+
+        }
+    }
+}
