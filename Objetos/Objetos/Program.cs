1575224f33e80ebafab17f720c142511a09a3dc5
arrate7 <43437355+arrate7@users.noreply.github.com>
2019-05-14 16:23:44 +0200

    .

--- Objetos/Objetos/Program.cs
+++ Objetos/Objetos/Program.cs
@@ -6,27 +6,36 @@ namespace Objetos
     {
         static void Main(string[] args)
         {
+            //Creo el coche con todos los atributos
             Coche coche1 = new Coche("Seat", "Ibiza",5,98,"Gasolina");
+            //Creo otro coche con 2 atributos
             Coche coche2 = new Coche("Seat", "Ibiza");
+            //Creo otro coche vacio
             Coche coche3 = new Coche();
 
-
+            //Cambio el modelo del coche1
             coche1.SetModelo("León");
+            //Hago que el coche1 gire a la derecha
             coche1.GirarDerecha();
 
+            //Establezco el valor de los atributos del coche3 uno a uno
             coche3.SetMarca("Ferrari");
             coche3.SetModelo("F40");
             coche3.SetPuertas(2);
             coche3.SetCaballos(300);
             coche3.SetTipo("Gasolina");
+
+            //Copio en coche2 los datos del coche3  
             coche2 = coche3;
 
+            //Imprimo todos los datos del coche1
             Console.WriteLine("Coche: " + coche1.GetMarca() + " " + coche1.GetModelo());
             Console.WriteLine("***********************************************************");
             Console.WriteLine("Puertas: " + coche1.GetPuertas());
             Console.WriteLine("Caballos: " + coche1.GetCaballos());
             Console.WriteLine("Tipo: " + coche1.GetTipo());
 
+            //Imprimo todos los datos del coche2
             Console.WriteLine("Coche: " + coche3.GetMarca() + " " + coche3.GetModelo());
             Console.WriteLine("***********************************************************");
             Console.WriteLine("Puertas: " + coche3.GetPuertas());
