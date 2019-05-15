4ccc818220c3faa8ed2f9b3d15d44fd670a7c4c0
arrate7 <43437355+arrate7@users.noreply.github.com>
2019-05-15 13:40:44 +0200

    .

---
+++ Objetos/Ejercicio2/Numero.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio2
+{
+    class Numero
+    {
+        //atributo
+        private int valor;
+
+        //constructor vacío
+        public Numero()
+        {
+
+        }
+
+        //constructor con parámetro
+        public Numero(int valor)
+
+        {
+            this.valor = valor;
+        }
+
+
+        //GETTERS Y SETTERS
+
+        public int GetValor()
+        {
+            return valor;
+        }
+
+        public void SetValor(int valor)
+        {
+            this.valor = valor;
+        }
+
+        //OTROS MÉTODOS
+
+        public void Aniade()
+        {
+            valor++;
+        }
+
+        public void Resta()
+
+        {
+             valor--;
+
+        }
+
+        public int GetDoble()
+        {
+
+            return valor * 2;
+        }
+
+        public int GetTriple()
+        {
+
+            return valor * 3;
+        }
+
+        public void SetNumero()  //public void o public int¿?
+        {
+            valor = 0;
+        }
+    }
+}
---
+++ Objetos/Ejercicio2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numero n1 = new Numero();
+            Numero n2 = new Numero(4);
+
+            n1.Aniade();
+            Console.WriteLine(n1.GetValor());
+            Console.WriteLine(n2.GetTriple());
+
+            
+
+        }
+    }
+}
