ddd6a209e3f4054583a16fd4b1efb9b6f8868351
arrate7 <43437355+arrate7@users.noreply.github.com>
2019-05-17 17:00:03 +0200

    .

---
+++ List/Ejercicio1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> listaCompra = new List<string>();
+            listaCompra = CrearLista(listaCompra);
+
+            MostrarLista(listaCompra);
+            OrdenarLista(listaCompra);
+            BorrarDeLista(listaCompra,"Pera");
+            Console.WriteLine("Cantidad de elementos: " + ContarElementos(listaCompra));
+        }
+
+        public static List<string> CrearLista(List<string> listaCompra)
+        {
+            listaCompra.Add("Peras");
+            listaCompra.Add("Manzanas");
+            listaCompra.Add("Arroz");
+           
+
+
+            return listaCompra;
+        }
+
+        public static void MostrarLista(List<string> lista)
+        {
+            foreach (string producto in lista)
+            {
+                Console.WriteLine(producto);
+            }
+        }
+
+        public static void OrdenarLista(List<string> lista)
+        {
+            lista.Sort();
+            MostrarLista(lista);
+        }
+
+        public static void BorrarDeLista(List<string>lista,string producto)
+        {
+            
+            if(lista.Remove(producto))
+            {
+                Console.WriteLine(producto + " se ha borrado.");
+            }
+            else
+            {
+
+                Console.WriteLine(producto + " no se encuentra en la lista");
+            }
+
+            MostrarLista(lista);
+        }
+
+        public static int ContarElementos(List<string> lista)
+        {
+            return lista.Count;
+        }
+    }
+}
+
---
+++ List/Ejercicio2/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> ints = new List<int>();
+            List<double> doubles = new List<double>();
+
+            double num;
+
+            do
+            {
+                Console.WriteLine("Introduce un número");
+                num = Convert.ToDouble(Console.ReadLine());
+
+                //if (num % 1 == 0)
+                //{
+                //    ints.Add(Convert.ToInt32(num));
+                //}
+                //else
+                //{
+                //    doubles.Add(num);
+                //}
+
+                if (num >= 0)
+                {
+
+                    if (Convert.ToString(num).Contains(","))
+                    {
+                        doubles.Add(num);
+                    }
+                    else
+                    {
+                        ints.Add(Convert.ToInt32(num));
+                    }
+                }
+
+            } while (num >= 0);
+
+            Console.WriteLine("Cantidad de números decimales: " + doubles.Count);
+            Console.WriteLine("Cantidad de números enteros: " + ints.Count);
+
+            SumarEnteros(ints);
+            SacarMedia(ints, doubles);
+        }
+
+        public static int SumarEnteros(List<int> ints)
+        {
+            int suma = 0;
+
+            foreach (int entero in ints)
+            {
+                suma += entero;
+            }
+            Console.WriteLine("La suma de los números enteros es: " + suma);
+
+            return suma;
+
+        }
+
+        public static void SacarMedia(List<int> ints, List<double> doubles)
+        {
+            int sumaEnteros = SumarEnteros(ints);
+            double sumaDoubles = 0;
+            double media;
+
+            foreach (double deci in doubles)
+            {
+                sumaDoubles += deci;
+            }
+
+            media = (sumaEnteros + sumaDoubles) / (ints.Count + doubles.Count);
+            Console.WriteLine("La media es: " + media);
+        }
+    }
+}
