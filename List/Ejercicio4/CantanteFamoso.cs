ee379849e2f64b01224ea54eca71207d15d84e99
arrate7 <43437355+arrate7@users.noreply.github.com>
2019-05-20 12:24:14 +0200

    .

---
+++ List/Ejercicio4/CantanteFamoso.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio4
+{
+    class CantanteFamoso
+    {
+        public string Nombre { get; set; }
+        public string Disco { get; set; }
+
+        public CantanteFamoso(string nombre, string disco)
+        {
+            Nombre = nombre;
+            Disco = disco;
+        }
+
+        public CantanteFamoso()
+        {
+        }
+
+        public void MostrarDatos()
+        {
+            Console.WriteLine("Nombre: " + Nombre + " Disco con más ventas: " + Disco);
+        }
+    }
+}
---
+++ List/Ejercicio4/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ejercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //OPCION 1
+            //List<CantanteFamoso> listaCantantesFamosos = new List<CantanteFamoso>()
+            //{
+            //    new CantanteFamoso("Ed Sheeran","Happier"),
+            //    new CantanteFamoso("Rihanna","Umbrella")
+            //};
+
+            //OPCION 2
+            //List<CantanteFamoso> listaCantantesFamosos = new List<CantanteFamoso>();
+            //CantanteFamoso c1 = new CantanteFamoso("Ed Sheeran", "Happier");
+            //CantanteFamoso c2 = new CantanteFamoso("Rihanna", "Umbrella");
+            //listaCantantesFamosos.Add(c1);
+            //listaCantantesFamosos.Add(c2);
+
+            //OPCION 3
+            CantanteFamoso c1 = new CantanteFamoso("Ed Sheeran", "Happier");
+            CantanteFamoso c2 = new CantanteFamoso("Rihanna", "Umbrella");
+            List<CantanteFamoso> listaCantantesFamosos = new List<CantanteFamoso>()
+            {
+                c1,c2
+            };
+
+            AnadirCantante(listaCantantesFamosos);
+            MostrarLista(listaCantantesFamosos);
+
+        }
+
+        public static void AnadirCantante(List<CantanteFamoso> cantantes)
+        {
+            string respuesta;
+            do
+            {
+                Console.WriteLine("Nombre del artista:");
+                string nombre = Console.ReadLine();
+                Console.WriteLine("Disco del artista:");
+                string disco = Console.ReadLine();
+
+                CantanteFamoso c1 = new CantanteFamoso(nombre, disco);
+                cantantes.Add(c1);
+
+                MostrarLista(cantantes);
+
+                Console.WriteLine("¿Quieres añadir otro (s/n)?");
+                respuesta = Console.ReadLine().ToLower();
+
+
+            } while (respuesta == "s");
+
+        }
+
+        public static void MostrarLista(List<CantanteFamoso> cantantes)
+        {
+            foreach (CantanteFamoso cantante in cantantes)
+            {
+                cantante.MostrarDatos();
+            }
+        }
+    }
+}
