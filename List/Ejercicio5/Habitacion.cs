562f00148ecf4fed1bf9d92dcbbaa8df7378952e
arrate7 <43437355+arrate7@users.noreply.github.com>
2019-05-20 15:11:22 +0200

    .

---
+++ List/Ejercicio5/Habitacion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio5
+{
+    class Habitacion
+    {
+        public int ID { get; set; }
+        public string Estado { get; set; }
+
+        public Habitacion(int iD, string estado)
+        {
+            ID = iD;
+            Estado = estado;
+        }
+
+        public Habitacion()
+        {
+        }
+    }
+}
---
+++ List/Ejercicio5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace Ejercicio5
+{
+    class Program
+    {
+        static SqlConnection connection = new SqlConnection("Data source=DESKTOP-QEAKD04\\SQLEXPRESS;Initial Catalog=Hotel_Transilvania;Integrated Security=True");
+        static void Main(string[] args)
+        {
+            List<Habitacion> habitaciones = new List<Habitacion>();
+
+            connection.Open();
+            string query = "SELECT * FROM Habitaciones";
+            SqlCommand comando = new SqlCommand(query, connection);
+            SqlDataReader registros = comando.ExecuteReader();
+
+            while (registros.Read())
+            {
+                //Habitacion h1 = new Habitacion(Convert.ToInt32(registros[0].ToString()), registros[1].ToString());
+                //Habitacion h1 = new Habitacion();
+                //h1.ID = Convert.ToInt32(registros[0].ToString());
+                //h1.Estado = registros[1].ToString();
+                //habitaciones.Add(h1);
+
+                habitaciones.Add(new Habitacion(Convert.ToInt32(registros[0].ToString()), registros[1].ToString()));
+            }
+
+            foreach(Habitacion habitacion in habitaciones)
+            {
+                Console.WriteLine(habitacion.ID + " " + habitacion.Estado);
+            }
+
+        }
+    }
+}
